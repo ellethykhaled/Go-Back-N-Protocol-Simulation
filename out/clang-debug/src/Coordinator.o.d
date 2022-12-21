e5a4e96bccfb44adffc4166f483089b74b87c4b1
ellethykhaled-github <ellethykhaled@yahoo.com>
2022-12-21 15:45:07 +0200

    Initialization Message

---
+++ out/clang-debug/src/Coordinator.o.d
@@ -0,0 +1,6 @@
+../out/clang-debug/src/Coordinator.o: Coordinator.cc Coordinator.h \
+  D:/Programs/omnetpp-6.0.1/include/omnetpp.h
+
+Coordinator.h:
+
+D:/Programs/omnetpp-6.0.1/include/omnetpp.h:
--- src/Coordinator.cc
+++ src/Coordinator.cc
@@ -1,28 +1,84 @@
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-// 
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Lesser General Public License for more details.
-// 
-// You should have received a copy of the GNU Lesser General Public License
-// along with this program.  If not, see http://www.gnu.org/licenses/.
-// 
-
 #include "Coordinator.h"
+#include "InitMessage_m.h"
 
 Define_Module(Coordinator);
 
 void Coordinator::initialize()
 {
-    // TODO - Generated method body
+    setParameters();
+    int startTime;
+    int startingNodeNumber = readCoordinatorFile(startTime);
+
+    initializeNodes(startTime, startingNodeNumber);
 }
 
 void Coordinator::handleMessage(cMessage *msg)
 {
-    // TODO - Generated method body
+}
+
+void Coordinator::initializeNodes(int startTime, int startingNodeNumber)
+{
+    InitMessage * initMsgSender = new InitMessage("Sender Initialization");
+    initMsgSender->setWS(WS);
+    initMsgSender->setTO(TO);
+    initMsgSender->setPT(PT);
+    initMsgSender->setTD(TD);
+    initMsgSender->setED(ED);
+    initMsgSender->setDD(DD);
+    initMsgSender->setStartTime(startTime);
+    initMsgSender->setStartingNode(startingNodeNumber);
+
+    InitMessage * initMsgReceiver = new InitMessage("Receiver Initialization");
+    initMsgReceiver->setPT(PT);
+    initMsgReceiver->setTD(TD);
+    initMsgReceiver->setLP(LP);
+    initMsgReceiver->setStartingNode(startingNodeNumber);
+
+    if (startingNodeNumber == 0){
+        send(initMsgSender, "node0Out");
+        send(initMsgReceiver, "node1Out");
+    }
+    else
+    {
+        send(initMsgSender, "node1Out");
+        send(initMsgReceiver, "node0Out");
+    }
+}
+
+void Coordinator::setParameters()
+{
+    // Reading the parameters from the network (in package.ned)
+    WS = this->getParentModule()->par("WS");
+    TO = this->getParentModule()->par("TO");
+    PT = this->getParentModule()->par("PT");
+    TD = this->getParentModule()->par("TD");
+    ED = this->getParentModule()->par("ED");
+    DD = this->getParentModule()->par("DD");
+    LP = this->getParentModule()->par("LP");
+}
+
+int readCoordinatorFile(int &startTime)
+{
+    std::string fileName = "..\\inputFiles\\coordinator.txt";
+    std::ifstream coordinatorFile(fileName);
+    std::string firstLine;
+    getline(coordinatorFile, firstLine);
+    coordinatorFile.close();
+
+    std::string number = "";
+    bool isFirst = true;
+    int startingNodeNumber;
+    for (auto c : firstLine)
+        if (c == ' ')
+        {
+            if (isFirst)
+                startingNodeNumber = stoi(number);
+            else
+                startTime = stoi(number);
+            isFirst = false;
+            number = "";
+        }
+        else
+            number = number + c;
+    return startingNodeNumber;
 }
